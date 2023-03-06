fc7f744e213b31fe3cb0cf9805a7a22caff04e8c
LiamHjalmarsson <Liam.hjalmarsson@hotmail.com>
2023-03-06 10:46:50 +0100

    trying to make it work with changein button

---
+++ components/component_buttons.js
@@ -0,0 +1,18 @@
+import { setDifficulty } from "../js/game_functions.js";
+
+export function levelButtons () {
+    const easyBtn = document.getElementById('easy-btn');
+    easyBtn.addEventListener('click', () => {
+        setDifficulty('easy');
+    });
+    
+    const mediumBtn = document.getElementById('medium-btn');
+    mediumBtn.addEventListener('click', () => {
+        setDifficulty('medium');
+    });
+    
+    const hardBtn = document.getElementById('hard-btn');
+    hardBtn.addEventListener('click', () => {
+        setDifficulty('hard');
+    });
+}
